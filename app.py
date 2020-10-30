5a51ae44232dcd256cd4fb3a0736f969f584330d
LostReny <isabelleribaldo@hotmail.com>
2020-10-30 19:17:41 +0000

    Trazendo o index hmtl

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html')
 
+
+@app.route('/lost')
+def lost():
+    return "Hello lost!!"
